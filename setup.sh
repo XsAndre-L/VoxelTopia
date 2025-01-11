2956cb9b766a0a1a721c83f85c91bd9bceb8a34f
xsandre-l <ahloubser12@gmail.com>
2025-01-11 18:59:21 +0200

    build should be easy now

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Source the colors.sh file
+source ./util/terminal/bash_color.sh
+
+# Get the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Set VCPKG_ROOT relative to the script's directory
+VCPKG_ROOT="$SCRIPT_DIR/vcpkg"
+export VCPKG_ROOT
+# CMAKE_TOOLCHAIN_FILE": "${env:VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
+
+# Set the toolchain file path
+CMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
+export CMAKE_TOOLCHAIN_FILE
+
+cd ./vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg install zlib zstd curl[winssl] openal-soft libvorbis libogg libjpeg-turbo sqlite3 freetype luajit gmp jsoncpp gettext[tools] sdl2 --triplet x64-windows --recurse
+
+# cd ..
+# cmake --preset Debug -S . -B build/debug
+# Automate later.... TODO
+
+echo -e "${GREEN}Done${RESET}"
+echo -e "${CYAN}Now set ${MAGENTA}${BOLD}CMake: Select Configure Preset ${RESET}${CYAN}to '${BOLD}Debug${RESET}${CYAN}'${RESET}"
+echo -e "${CYAN}Then ${MAGENTA}${BOLD}CMake: Configure${RESET}"
+echo -e "${CYAN}And finally ${MAGENTA}${BOLD}CMake: Build All${RESET}"
---
+++ util/terminal/bash_color.sh
@@ -0,0 +1,26 @@
+# colors.sh
+
+# Reset formatting
+export RESET="\033[0m"
+
+# Text colors
+export RED="\033[31m"
+export GREEN="\033[32m"
+export YELLOW="\033[33m"
+export BLUE="\033[34m"
+export MAGENTA="\033[35m"
+export CYAN="\033[36m"
+export WHITE="\033[37m"
+
+# Background colors
+export BG_RED="\033[41m"
+export BG_GREEN="\033[42m"
+export BG_YELLOW="\033[43m"
+export BG_BLUE="\033[44m"
+export BG_MAGENTA="\033[45m"
+export BG_CYAN="\033[46m"
+export BG_WHITE="\033[47m"
+
+# Text formatting
+export BOLD="\033[1m"
+export UNDERLINE="\033[4m"
